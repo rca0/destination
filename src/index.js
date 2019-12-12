0d30c2176cad4b46bad32094303765871039afbc
Ruan Arcega <ruanarcega@gmail.com>
2019-12-12 02:54:36 -0300

    feat: configure location

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import MapView from 'react-native-maps';
 
 import { View } from 'react-native';
 
-const App = () => <View />;
+const App = () => (
+    <View stype={{ flex: 1 }}>
+        <MapView
+            stype={{ flex: 1 }}
+            region={{
+                latitude: -26.265518,
+                longitude: -48.877581,
+                latitudeDelta: 0.0143,
+                longitudeDelta: 0.034,
+            }}
+            showsUserLocation
+            loadingEnabled
+        />
+    </ View>
+);
 
 export default App;
