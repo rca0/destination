9026e3496d7b15112fb13ebb5e9df62f94e40596
Ruan Arcega <ruanarcega@gmail.com>
2019-12-17 00:17:39 -0300

    feat: add map component

---
+++ src/components/Map/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+import MapView from 'react-native-maps';
+
+export default class Map extends Component {
+    state = {
+        region: null
+    };
+
+    // render component in screen
+    async componentDidMount() {
+        // callback situation
+        navigator.geolocation.getCurrentPosition(
+            ({ coords: { latitude, longitude } }) => {
+                this.setState({
+                    region: {
+                        latitude,
+                        longitude,
+                        latitudeDelta: 0.0143,
+                        longitudeDelta: 0.0134,
+                    }
+                 });
+            }, // success
+            () => {}, // error
+            {
+                timeout: 2000,
+                enableHighAccuracy: true, // ways to get geolocation (1st wifi after gps)
+                maximumAge: 1000, // cache location
+            } // user positon
+        )
+    }
+
+    render() {
+        const { region } = this.state;
+
+        return (
+        <View stype={{ flex: 1 }}>
+            <MapView
+                stype={{ flex: 1 }}
+                region={region}
+                showsUserLocation
+                loadingEnabled
+            />
+        </ View>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import Map from './components/Map';
 
-import { View } from 'react-native';
-
-const App = () => (
-    <View stype={{ flex: 1 }}>
-        <MapView
-            stype={{ flex: 1 }}
-            region={{
-                latitude: -26.265518,
-                longitude: -48.877581,
-                latitudeDelta: 0.0143,
-                longitudeDelta: 0.034,
-            }}
-            showsUserLocation
-            loadingEnabled
-        />
-    </ View>
-);
+const App = () => <Map />;
 
 export default App;
